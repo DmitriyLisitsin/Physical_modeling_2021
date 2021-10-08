72d6de63dca1a8098c9d4445d19b331f8ffe8c00
Oleg20502 <oleg20502@gmail.com>
2021-10-08 11:22:44 +0300

    Modified Simple_task_2 programs

--- My_work_3/Simple_task_2.cpp
+++ My_work_3/Simple_task_2.cpp
@@ -6,30 +6,22 @@
 int main()
 {
     int T;
-    double dt, k, R;
+    double dt, k, R, v0;
 
-    T = 100;
-    R = 3400000;
+    T = 10;
+    R = 6400000-10;
+    v0 = 10.0;
     dt = 0.1;
-    k = 1.0d;
-
+    k = 3.0d;
     int iter = T/dt;
-//    std::vector<double> a(iter, 0);
-//    std::vector<double> b(iter, 0);
-//    std::vector<double> time(iter, 0);
-//    a[0] = R;
-//    for(int i=0; i<iter-1; ++i){
-//        a[i+1] = a[i] + b[i]*dt;
-//        b[i+1] = b[i] - a[i]*k*dt;
-//        time[i+1] = time[i] + dt;
-//    }
     double a_old = R;
-    double b_old = 0.0;
+    double b_old = v0;
     double a_new, b_new, t = 0.0;
 
     std::ofstream out;
     out.open("Data.txt");
     if (out.is_open()){
+        out << T << ';' << dt << ';' << k << ';' << R << ';' << v0 << '\n';
         for(int i=0; i<iter; ++i){
             a_new = a_old + b_old*dt;
             b_new = b_old - a_old*k*dt;
--- My_work_3/Simple_task_2_plots.py
+++ My_work_3/Simple_task_2_plots.py
@@ -2,11 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 D = np.array([])
+T, dt, k, R = 0, 0, 0, 0
 with open("Data.txt", "r") as file:
     data1 = []
+    i = 0
     for line in file:
-        data1 = list(map(float, line.split(';')))
-        D = np.append(D, data1, axis = 0)
+        if i:
+            data1 = list(map(float, line.split(';')))
+            D = np.append(D, data1, axis = 0)
+        else:
+            T, dt, k, R, v0 = list(map(float, line.split(';')))
+        i += 1
 D = D.reshape(D.size//3, 3)
 
 t = D[:, 0]
@@ -16,7 +22,7 @@ b = D[:, 2]
 plt.plot(a, b)
 plt.show()
 
-#plt.plot(t, a**2 + b**2/2)
-plt.plot(t, a)
+plt.plot(t, a**2 + b**2/k)
+#plt.plot(t, a)
 #plt.plot(t, b)
 plt.show()
