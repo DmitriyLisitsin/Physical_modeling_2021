b96f9e30f348fc1487fb7269f3944bd3223aa7be
Oleg20502 <oleg20502@gmail.com>
2021-10-10 10:51:04 +0300

    Lesson5 update

--- My_work_3/Simple_task_2.cpp
+++ My_work_3/Simple_task_2.cpp
@@ -2,34 +2,68 @@
 #include <cmath>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <iomanip>
 
 int main()
 {
     int T;
     double dt, k, R, v0;
 
-    T = 10;
-    R = 6400000-10;
-    v0 = 10.0;
-    dt = 0.1;
-    k = 3.0d;
-    int iter = T/dt;
+    int regime = 0;
+
+    T = 1;
+    R = 6400;
+    v0 = 0;
+    dt = 1.0/pow(10, 9);
+    k = 1.0d;
+    double time_step = 0.1;
+    if (regime == 1){
+        T = 1;
+        R = 6400;
+        v0 = 0;
+        k = 1.0d;
+        dt = 1.0/pow(10, 9);
+    }
+
+    long long int iter = T/dt;
+    long long int write = time_step/dt;
     double a_old = R;
     double b_old = v0;
     double a_new, b_new, t = 0.0;
 
     std::ofstream out;
-    out.open("Data.txt");
+    std::string file1 = "Data.txt";
+    std::string file2 = "Energy.txt";
+
+    if (regime == 0){
+        out.open(file1);
+    }
+    else if (regime == 1){
+        out.open(file2, std::ios::app);
+    }
     if (out.is_open()){
-        out << T << ';' << dt << ';' << k << ';' << R << ';' << v0 << '\n';
+
+        out << std::setprecision(14);
+        out << std::scientific;
+        if (regime == 0){
+            out << T << ';' << dt << ';' << k << ';' << R << ';' << v0 << '\n';
+        }
+        std::cout << iter << std::endl;
+        std::cout << write << std::endl;
         for(int i=0; i<iter; ++i){
+            //std::cout << "I'm here" <<std::endl;
             a_new = a_old + b_old*dt;
             b_new = b_old - a_old*k*dt;
-            out << t << ';' << a_old << ';' << b_old << '\n';
+            if (regime == 0 && i % write == 0)
+                out << t << ';' << a_old << ';' << b_old << '\n';
             a_old = a_new;
             b_old = b_new;
             t += dt;
         }
+        if (regime == 1)
+            //std::cout << (k*pow(a_old, 2) + pow(b_old, 2))/2 << '\n';
+            out << dt << ';' << (k*pow(a_old, 2) + pow(b_old, 2))/2 << '\n';
     }
     out.close();
     std::cout << "finished" << '\n';
--- My_work_3/Simple_task_2_plots.py
+++ My_work_3/Simple_task_2_plots.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 D = np.array([])
 T, dt, k, R = 0, 0, 0, 0
+
+#D = np.fromfile("Data.txt", 'float', )
+
 with open("Data.txt", "r") as file:
     data1 = []
     i = 0
@@ -14,15 +17,33 @@ with open("Data.txt", "r") as file:
             T, dt, k, R, v0 = list(map(float, line.split(';')))
         i += 1
 D = D.reshape(D.size//3, 3)
-
 t = D[:, 0]
 a = D[:, 1]
 b = D[:, 2]
 
-plt.plot(a, b)
-plt.show()
+D = np.array([])
+with open("Energy.txt", "r") as file:
+    data1 = []
+    for line in file:
+        data1 = list(map(float, line.split(';')))
+        D = np.append(D, data1, axis = 0)
+D = D.reshape(D.size//2, 2)
+Dt = D[:, 0]
+E = D[:, 1]
 
-plt.plot(t, a**2 + b**2/k)
-#plt.plot(t, a)
-#plt.plot(t, b)
+plt.plot(t, (k*a**2 + b**2)/2)
+plt.title('Energy from t', fontsize = 12)
+plt.show()
+plt.plot(t, a)
+plt.title('x from t', fontsize = 12)
+plt.show()
+plt.plot(t, b)
+plt.title('v from t', fontsize = 12)
+plt.show()
+plt.plot(a, b)
+plt.title('v from x', fontsize = 12)
+plt.show()
+plt.scatter(np.log10(Dt), np.log10(E))
+#plt.scatter(Dt, E)
+plt.title('E from dt, t0 = ' + str(T), fontsize = 12)
 plt.show()
