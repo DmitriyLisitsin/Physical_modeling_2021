51bedd95ffa1688f1c5c1ce2d19aa75906cccdbf
Oleg20502 <oleg20502@gmail.com>
2021-10-16 16:33:18 +0300

    Update Simple_task_2.cpp

--- My_work_3/Simple_task_2.cpp
+++ My_work_3/Simple_task_2.cpp
@@ -12,22 +12,24 @@ int main()
 
     int regime = 0;
 
+
+    double time_step = 0.05;
     T = 1;
     R = 6400;
     v0 = 0;
-    dt = 1.0/pow(10, 9);
+    dt = 1.0/pow(10, 6);
     k = 1.0d;
-    double time_step = 0.1;
+
     if (regime == 1){
         T = 1;
         R = 6400;
         v0 = 0;
         k = 1.0d;
-        dt = 1.0/pow(10, 9);
+        dt = 1.0/pow(10, 6);
     }
 
-    long long int iter = T/dt;
-    long long int write = time_step/dt;
+    long long int iter = static_cast<long long int> (T/dt);
+    long long int write = static_cast<long long int> (time_step/dt);
     double a_old = R;
     double b_old = v0;
     double a_new, b_new, t = 0.0;
@@ -49,23 +51,22 @@ int main()
         if (regime == 0){
             out << T << ';' << dt << ';' << k << ';' << R << ';' << v0 << '\n';
         }
-        std::cout << iter << std::endl;
-        std::cout << write << std::endl;
-        for(int i=0; i<iter; ++i){
-            //std::cout << "I'm here" <<std::endl;
+        for(long long int i=0; i<iter; ++i){
             a_new = a_old + b_old*dt;
             b_new = b_old - a_old*k*dt;
-            if (regime == 0 && i % write == 0)
+            if (regime == 0 && i % write == 0){
+                std::cout << "Write" << '\n';
                 out << t << ';' << a_old << ';' << b_old << '\n';
+            }
             a_old = a_new;
             b_old = b_new;
             t += dt;
         }
-        if (regime == 1)
-            //std::cout << (k*pow(a_old, 2) + pow(b_old, 2))/2 << '\n';
+        if (regime == 1){
             out << dt << ';' << (k*pow(a_old, 2) + pow(b_old, 2))/2 << '\n';
+        }
     }
     out.close();
-    std::cout << "finished" << '\n';
+    std::cout << "Finished" << '\n';
     return 0;
 }
